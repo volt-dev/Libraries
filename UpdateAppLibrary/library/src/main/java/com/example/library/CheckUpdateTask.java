dd99101ccbd229edc89167ad94b6aee1d7b5b936
danilo <dcolazo@voltmotors.com.ar>
2020-12-23 16:11:31 -0300

    Ahora la verificación se hace por realtime database.

--- UpdateAppLibrary/library/src/main/java/com/example/library/CheckUpdateTask.java
+++ UpdateAppLibrary/library/src/main/java/com/example/library/CheckUpdateTask.java
@@ -3,20 +3,26 @@ package com.example.library;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import androidx.annotation.NonNull;
 
-class CheckUpdateTask extends AsyncTask<Void, Void, String> {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+class CheckUpdateTask extends AsyncTask<Void, Void, Void> {
 
     private ProgressDialog dialog;
     private Context mContext;
     private int mType;
     private boolean mShowProgressDialog;
     private String JSONurl;
+    private String apkUrl;
+    private int apkCode = 0;
 //    private static final String url = com.example.library.Constants.UPDATE_URL;
 
     CheckUpdateTask(Context context, int type, boolean showProgressDialog, String JSONurl) {
@@ -39,48 +45,63 @@ class CheckUpdateTask extends AsyncTask<Void, Void, String> {
 
 
     @Override
-    protected void onPostExecute(String result) {
+    protected void onPostExecute(Void result) {
 
         if (dialog != null && dialog.isShowing()) {
             dialog.dismiss();
         }
 
-        if (!TextUtils.isEmpty(result)) {
-            parseJson(result);
-        }
+//        if (!TextUtils.isEmpty()) {
+        parseJson();
+//        }
     }
 
-    private void parseJson(String result) {
-        try {
-            // Se obtiene el String en el proceso doInBackground y se genera un nuevo JSON.
-            JSONObject obj = new JSONObject(result);
-            // Extren los Strings del JSON.
-            String updateMessage = obj.getString(com.example.library.Constants.APK_UPDATE_CONTENT);
-            Log.i("APPD", "updateMessage: "+obj.getString(com.example.library.Constants.APK_UPDATE_CONTENT));
+    private void parseJson() {
 
-            String apkUrl = obj.getString(com.example.library.Constants.APK_DOWNLOAD_URL);
-            Log.i("APPD", "apkUrl: " +obj.getString(com.example.library.Constants.APK_UPDATE_CONTENT));
 
-            // Se extrae el código de versión del JSON.
-            int apkCode = obj.getInt(com.example.library.Constants.APK_VERSION_CODE);
-            Log.i("APPD", "apkCode: "+apkCode);
-            // Se extrae el código de versión de la app actual.
-            int versionCode = com.example.library.AppUtils.getVersionCode(mContext);
-            Log.i("APPD", "versioncode: "+versionCode);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
 
-            // Se comparan y se toma una decisión con respecto al resultado.
-            if (apkCode > versionCode) {
-                if (mType == com.example.library.Constants.TYPE_NOTIFICATION) {
-                    new com.example.library.NotificationHelper(mContext).showNotification(updateMessage, apkUrl);
-                } else if (mType == com.example.library.Constants.TYPE_DIALOG) {
-                    showDialog(mContext, updateMessage, apkUrl);
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    switch (snapshot1.getKey()) {
+                        case "url":
+                            apkUrl = (String) snapshot1.getValue();
+                            break;
+                        case "versionCode":
+                            String aux = "" + snapshot1.getValue();
+                            apkCode = Integer.parseInt(aux);
+                            break;
+                    }
                 }
-            } else if (mShowProgressDialog) {
-                Toast.makeText(mContext, mContext.getString(R.string.android_auto_update_toast_no_new_update), Toast.LENGTH_SHORT).show();
+                Log.i("TAGS", "url: " + apkUrl);
+                Log.i("TAGS", "versionCode: " + apkCode);
             }
 
-        } catch (JSONException e) {
-            Log.e(com.example.library.Constants.TAG, "parse json error");
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        String updateMessage = "";
+        Log.i("APPD", "apkCode: " + apkCode);
+
+        // Se extrae el código de versión de la app actual.
+        int versionCode = AppUtils.getVersionCode(mContext);
+        Log.i("APPD", "versioncode: " + versionCode);
+
+        // Se comparan y se toma una decisión con respecto al resultado.
+        if (apkCode > versionCode) {
+            if (mType == Constants.TYPE_NOTIFICATION) {
+                new NotificationHelper(mContext).showNotification(updateMessage, apkUrl);
+            } else if (mType == Constants.TYPE_DIALOG) {
+                showDialog(mContext, updateMessage, apkUrl);
+            }
+        } else if (mShowProgressDialog) {
+            Toast.makeText(mContext, mContext.getString(R.string.android_auto_update_toast_no_new_update), Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -94,7 +115,8 @@ class CheckUpdateTask extends AsyncTask<Void, Void, String> {
 
 
     @Override
-    protected String doInBackground(Void... args) {
-        return com.example.library.HttpUtils.get(JSONurl);
+    protected Void doInBackground(Void... args) {
+        return null;
+//        return com.example.library.HttpUtils.get(JSONurl);
     }
 }
